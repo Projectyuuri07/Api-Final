f6d4518c76183df7ab818dcc655ebc2c9a6e97c7
Projectyuuri07 <projectyuuri07@gmail.com>
2023-11-30 13:11:04 -0300

    Update

--- venv/Projeto/JavaScript/game.js
+++ venv/Projeto/JavaScript/game.js
@@ -13,3 +13,21 @@ var cards = document.querySelectorAll('.card');
     card.classList.toggle('is-flipped');
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const backgroundMusic = document.getElementById('backgroundMusic');
+  const toggleMusicButton = document.getElementById('toggleMusicButton');
+  const muteIcon = document.getElementById('mute');
+
+  toggleMusicButton.addEventListener('click', function () {
+      if (backgroundMusic.paused) {
+          backgroundMusic.play();
+          muteIcon.classList.remove('bi-volume-mute');
+          muteIcon.classList.add('bi-volume-up');
+      } else {
+          backgroundMusic.pause();
+          muteIcon.classList.remove('bi-volume-up');
+          muteIcon.classList.add('bi-volume-mute');
+      }
+  });
+});
