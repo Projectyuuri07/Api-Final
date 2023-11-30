d5882d1a7b3d0f4419a837df80e0c0211d03c6dc
Projectyuuri07 <projectyuuri07@gmail.com>
2023-11-30 08:13:23 -0300

    Update

--- venv/app.py
+++ venv/app.py
@@ -1,57 +1,45 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
-import os
+import random
 
 ########### MATRIZ 3X4 ###########
 
 matriz = {}
-matriz[(0,0)] = 0
-matriz[(0,1)] = 0
-matriz[(0,2)] = 0
-matriz[(1,0)] = 0
-matriz[(1,1)] = 0
-matriz[(1,2)] = 0
-matriz[(2,0)] = 0
-matriz[(2,1)] = 0
-matriz[(2,2)] = 0
-matriz[(3,0)] = 0
-matriz[(3,1)] = 0
-matriz[(3,2)] = 0
 
-print(matriz[(0,0)],
-matriz[(0,1)],
-matriz[(0,2)])
+matriz = {(0, 0): 'cell.png', (0, 1): 'android18.png', (0, 2): 'frizza.png',
+          (1, 0): 'gohan.png', (1, 1): 'goku.png', (1, 2): 'kuririn.png',
+          (2, 0): 'cell.png', (2, 1): 'android18.png', (2, 2): 'frizza.png',
+          (3, 0): 'gohan.png', (3, 1): 'goku.png', (3, 2): 'kuririn.png'}
 
-print(matriz[(1,0)],
-matriz[(1,1)],
-matriz[(1,2)])
-
-print(matriz[(2,0)],
-matriz[(2,1)],
-matriz[(2,2)])
-
-print(matriz[(3,0)],
-matriz[(3,1)],
-matriz[(3,2)])
+cartas_viradas = []
 
 ##################################
 
 app = Flask(__name__)
 CORS(app)
 
-#não faço ideia de como fazer pra listar IMAGENS com JSON
 @app.route("/list", methods=['GET'])
-def sortearCartas():    
-    diretorio = 'imagens'
-    
-    # OS.LISTDIR -> listar todo o diretório de imagens
-    imagens = [f for f in os.listdir(diretorio) if os.path.isfile(os.path.join(diretorio, f))]
-    
-    # Criar uma lista de dicionários contendo o nome e o caminho de cada imagem
-    lista_imagens = [{'nome': imagem, 'caminho': f'/imagens/{imagem}'} for imagem in imagens]
+def listarCartas():
+    #pegar as posicoes da matriz e coloca-los numa lista
+    posicoes = list(matriz.keys())
 
-    return jsonify(lista_imagens)
+    # vai embaralhar as cartas
+    random.shuffle(posicoes)
+    
+    # retornando um JSON
+    cartas = [{'id': id, 'posicao': pos, 'imagem': matriz[pos]} for id, pos in enumerate(posicoes, start=1)]
+    return jsonify(cartas)
+
+    
+
+@app.route("/add", methods=['PUT'])
+def loginJogador():
+    pass
+
+@app.route("/delete/<int:id>/<int:id>", methods=['DELETE'])
+def deletarCartas(idCarta1, idCarta2):
+    pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
